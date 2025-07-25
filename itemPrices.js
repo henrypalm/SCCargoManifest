166c9531f6b970545220baae3ceb77d658c3ee66
henrypalm <rgoodwin130@gmail.com>
2025-07-24 22:43:51 -0400

    Create itemPrices.js

---
+++ itemPrices.js
@@ -0,0 +1,6 @@
+// itemPrices.js
+const itemPrices = {
+    "Quantanium": { "Area18": 275, "Lorville": 260.5, "Orison": 280 },
+    "Bexalite": { "Area18": 180, "Lorville": 190, "Orison": 185.5 },
+    "RMC": { "Area18": 14.5, "Lorville": 15, "Orison": 15.2 }
+};
