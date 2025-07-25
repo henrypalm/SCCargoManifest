aa92435256ec49d97960d844cd2b7658abd7fb6d
henrypalm <rgoodwin130@gmail.com>
2025-07-24 23:24:44 -0400

    Update itemColors.js

--- itemColors.js
+++ itemColors.js
@@ -1,6 +1,18 @@
 // itemColors.js
 const itemColors = {
-    "Quantanium": "#FFD700",  // Gold
-    "Bexalite": "#00FF00",    // Lime green
-    "RMC": "#1E90FF"          // Dodger blue
+  "Stileron": "#FFD700",
+  "Quantanium": "#FFD700",
+  "Riccite": "#FFD700",
+  "Taranite": "#006400",
+  "Bexalite": "#006400",
+  "Gold": "#006400",
+  "Diamond": "#006400",
+  "Borase": "#ADD8E6",
+  "Laranite": "#ADD8E6",
+  "Beryl": "#ADD8E6",
+  "Agricium": "#ADD8E6",
+  "Hephaestanite": "#ADD8E6",
+  "Recycled Material Composite": "#FFD700",
+  "Construction Material": "#FF0000"
 };
+
